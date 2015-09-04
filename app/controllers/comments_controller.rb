1a54dec03dc609f82a6b05d70b09d545c06db942
cef2010 <cef2010@gmail.com>
2015-09-04 16:24:40 -0600

    Comments New/Selector reorder

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,24 @@
 class CommentsController < ApplicationController
   def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:post_id, :name, :body)
   end
 end
