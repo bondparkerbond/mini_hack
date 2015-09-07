963b4626ba8c5d0884a1e6ea3a283a4189485705
bondparkerbond <parkerbond@gmail.com>
2015-09-07 15:39:26 -0600

    Can create comment, no deletion yet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+
+  # def index
+  #   @ = Post.all.sort_by{|post| post.vote}.reverse
+  # end
+
   def new
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new
@@ -6,6 +11,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    # @comment = Comment.new(params[:id]) bad param
     @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to post_path(@post)
@@ -15,7 +21,29 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:post_id])
   end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   #@post = Post.find(params[:post_id])
+  #   @comment = Comment.find(params[:id])
+  #   @comment.destroy
+  #     redirect_to post_path(@comment.post_id)
+  #   # else
+  #   #   redirect_to post_path(@post)
+  #   # end
+  # end
 
   private
   def comment_params
